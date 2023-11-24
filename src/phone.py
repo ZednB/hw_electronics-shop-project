39c934dddfcb443f7bac6c2825287fe18edba8c1
Zubayr Bibulatov <Ru.Zubayr@gmail.com>
2023-11-24 09:03:45 +0300

    Done

--- src/phone.py
+++ src/phone.py
@@ -5,10 +5,18 @@ class Phone(Item):
 
     def __init__(self, name, price, quantity, number_of_sim):
         super().__init__(name, price, quantity)
-        self.number_of_sim = number_of_sim
+        self.__number_of_sim = number_of_sim
         self.all.append(self)
 
+    @property
+    def number_of_sim(self):
+        return self.__number_of_sim
+
+    @number_of_sim.setter
+    def number_of_sim(self, number_of_sim):
+        self.__number_of_sim = number_of_sim
+
     def __repr__(self):
-        return f"{self.__class__.__name__}('{self.name}', {self.price}, {self.quantity}, {self.number_of_sim})"
+        return f"{self.__class__.__name__}('{self.name}', {self.price}, {self.quantity}, {self.__number_of_sim})"
 
 
